9eae190aef5eb21912b0b66e8a92e7cc7a16068b
Supakan06 <66160033@go.buu.ac.th>
2025-02-10 23:01:26 +0700

    ตั้งค่า products เป็นอาร์เรย์ว่าง (ไม่มีสินค้า)

--- script.js
+++ script.js
@@ -163,6 +163,7 @@ function updateLastUpdated() {
 function clearAllData() {
     if (confirm("คุณแน่ใจหรือไม่ว่าต้องการล้างข้อมูลทั้งหมด?")) {
         localStorage.removeItem("products");
+        products = [];
     }
 }
 
